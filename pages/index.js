e6a1c4a320c40ce91dc0eb61875dc2080c80e261
Saurin Thakkar <saurin.thakkar13@gmail.com>
2022-12-02 21:41:06 +0530

    refact:updated client

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
 import Link from "next/link";
 import groq from "groq";
-import client from "./lib/page";
+import client from "./page";
 
 const Index = ({ posts }) => {
+  console.log("POL", posts);
   return (
     <div>
-      <h1>Jello world</h1>
+      <h1>Welcome to blog</h1>
+      {posts.length > 0 &&
+        posts.map(
+          ({ _id, title = "", slug = "", publishedAt = "" }) =>
+            slug && (
+              <li key={_id}>
+                <Link href="/post/slug" as={`/post/${slug.current}`}>
+                  {title}
+                </Link>{" "}
+                ({new Date(publishedAt).toDateString()})
+              </li>
+            )
+        )}
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const posts = await client.fetch(groq`
+  *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
+    `);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 
 export default Index;
